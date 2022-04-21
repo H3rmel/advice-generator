56be0b4563162b09c36eda6b300bd1f314ea2832
isaac.reginato <isaachermel@gmail.com>
2022-04-21 03:56:24 -0300

    static front-end done

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 // React JS
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // Main CSS
-import './index.css';
+import "./index.css";
 
 // App
-import App from './app/App';
+import App from "./app/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
